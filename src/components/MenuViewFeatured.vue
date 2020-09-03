06e88a28b35435602de4dd3a9a0c5f4fd03fc2ea
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2020-09-03 19:50:31 +0000

    Updated git ignore to allow for two step publishing on forge server.

--- src/components/MenuViewFeatured.vue
+++ src/components/MenuViewFeatured.vue
@@ -1,7 +1,7 @@
 <template>
   <a
     :href="url"
-    class="px-4 xl:px-2 py-2 xl:mr-0 xl:-ml-2 xl:-mr-2 cursor-pointer text-blue-800 font-medium hover:text-blue-800 hover:underline border border-gray-200 rounded-lg flex items-center min-h-16 text-lg leading-tight mb-4"
+    class="px-4 xl:px-2 py-2 xl:-ml-2 xl:-mr-2 cursor-pointer text-blue-800 font-medium hover:text-blue-800 hover:underline border border-gray-200 rounded-lg flex items-center min-h-16 text-lg leading-tight mb-4"
   >
     <slot name="icon"></slot>
 
