6c60dd08b91430128a0b25601b9630c8c25c5e05
root <remy.vanherweghem@parl.gc.ca>
2023-02-02 13:18:44 +0000

    chore: updated depencencies.

--- src/defineCustomPeatElement.js
+++ src/defineCustomPeatElement.js
@@ -1,10 +1,10 @@
 // Adapted from https://vueshowcase.com/question/how-to-use-vue-router-and-vuex-inside-custom-element-from-root
 import { defineCustomElement, h, createApp, getCurrentInstance } from 'vue'
 const language = document.documentElement.lang;
-import stl from './index.css'
+import stl from './index.css?inline'
 
 export const defineCustomPeatElement = (component, { plugins = [] }) =>
-defineCustomElement({
+  defineCustomElement({
     render: () => h(component),
     styles: [stl],
     props: {
@@ -17,7 +17,7 @@ defineCustomElement({
       const inst = getCurrentInstance()
       Object.assign(inst.appContext, app._context)
       Object.assign(inst.provides, app._context.provides)
-      
+
       return {
         language
       }
